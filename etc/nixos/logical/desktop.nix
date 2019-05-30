3075fdb788fa749d0d6c22603a2fd29810034626
Márton Boros <martonboros@gmail.com>
2019-05-30 17:27:00 +0000

    add progs, sway, gnome-keyring

--- etc/nixos/logical/desktop.nix
+++ etc/nixos/logical/desktop.nix
@@ -3,14 +3,15 @@
 {
   imports = [ ./graphical.nix ];
 
-  services.keybase.enable = true;
-  services.kbfs.enable = true;
-
+  programs.spacefm.enable = true;
+  programs.browserpass.enable = true;
   programs.chromium.enable = true;
   programs.chromium.extensions = [
     "cfhdojbkjhnklbpkdaibdccddilifddb" # Adblock Plus
     "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
   ];
+  programs.sway.enable = true;
+  programs.sway.extraPackages = with pkgs; [ swaylock swayidle xwayland rxvt_unicode dmenu i3status i3status-rust light ];
 
   #services.xserver.desktopManager.xfce.enable = true;
   #services.xserver.displayManager.lightdm.enable = true;
@@ -18,8 +19,11 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.libinput.enable = lib.mkForce false;
 
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
   environment.systemPackages = with pkgs; [
     chromium
+    browserpass
   ];
 
   # TODO:
